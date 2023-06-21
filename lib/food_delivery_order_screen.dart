cc585bdb536da0cad1027e644c7468b6ed13106e
beomq <beomdolq@gmail.com>
2023-06-21 16:25:23 +0900

    first commit

---
+++ lib/food_delivery_order_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FoodDeliveryOrderScreen extends StatelessWidget {
+  const FoodDeliveryOrderScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
